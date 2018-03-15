2b22e03cf927cd2a71aa2d9f05acc1425102b14a
LE Xuan Tien <lexuantien7617@outlook.com.vn>
2018-03-15 19:49:38 +0700

    test search bar

--- src/pages/CartScreen.js
+++ src/pages/CartScreen.js
@@ -1,17 +1,40 @@
 import React,{Component} from "react";
 import {
     View,
-    Text
+    Text,
+    TouchableWithoutFeedback,
+    TextInput
 } from "react-native"
 
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+
 export default class CartScreen extends Component {
-
-    
-
     render(){
         return(
             <View>
-                <Text>Cart Screen</Text>
+                <TouchableWithoutFeedback>
+                    <View style = {{ 
+                        marginTop: 5,
+                        marginHorizontal: 10,
+                        height: 30,
+                        flexDirection: "row", 
+                        alignItems: "center", 
+                        borderRadius:  50,
+                        backgroundColor: 'red'
+                         }} >
+                        <EvilIcons style = {{ marginLeft: 10 }} name = "search" size = { 25 } />
+                        <TextInput 
+                            placeholder = "What're you looking for ?" 
+                            underlineColorAndroid = "transparent"
+                            style = {{ 
+                                flex:1 ,
+                                height:30,
+                                textDecorationLine: "underline",
+                                
+                                }}
+                            />
+                    </View>
+                </TouchableWithoutFeedback>
             </View>
         );
     }
