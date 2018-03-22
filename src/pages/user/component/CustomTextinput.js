cc158c193e2a543ac06b885b96953bdbbe8e9d59
LE Xuan Tien <lexuantien7617@outlook.com.vn>
2018-03-22 23:20:30 +0700

    80% US + change profile

--- src/pages/user/component/CustomTextinput.js
+++ src/pages/user/component/CustomTextinput.js
@@ -5,6 +5,7 @@ import {
     Dimensions,
     Animated,
     TouchableOpacity,
+    TextInput
 } from "react-native"
 import PropTypes from "prop-types"
 import Entypo from 'react-native-vector-icons/Entypo';
@@ -13,7 +14,7 @@ const { height, width } = Dimensions.get("window");
 const fcenter_height=height*0.22;
 
 
-export default class CustomOption extends Component {
+export default class CustomTextinput extends Component {
     // custom some props
     static propTypes = {
         
@@ -26,14 +27,14 @@ export default class CustomOption extends Component {
         
         label: PropTypes.string.isRequired,
         labelStyle: Text.propTypes.style,
-        labelSize: PropTypes.number
-
+        labelSize: PropTypes.number,
+        hint: PropTypes.string
     }
 
     static defaultProps = {
         height: 30,
         iconColor: "#989898",
-        iconSize: 35,        
+        iconSize: 30,        
         label: "edit text",
         labelSize:17,
         cHeight:53,      
@@ -48,31 +49,27 @@ export default class CustomOption extends Component {
             cHeight,
             label,
             labelSize,
-            labelStyle            
+            labelStyle,
+            hint            
         } = this.props
         // get image without specific class
         const AnimatedIcon = Animated.createAnimatedComponent(iconClass);
         return(
-            <TouchableOpacity>
-                <View style = {{ justifyContent:"space-between", flexDirection: "row", height:cHeight, alignItems:"center", borderBottomWidth:1, borderColor:"#d4d3d0"}} >
-                    <View  style = {{ flexDirection: "row", alignItems:"center", justifyContent: "center"}}>
-                        <AnimatedIcon                    
-                            style = {{ width: width*0.12, marginLeft:10 }}
-                            name = {iconName}
-                            color = {iconColor}
-                            size = {iconSize}
-                            />
-                        <Text style = { [ labelStyle, {fontSize:labelSize } ] }>{ label }</Text>                     
-                    </View>           
-
-                    <Entypo 
-                        name = "chevron-small-right"
-                        color = "#989898"
-                        size = {30}
-                        />                
-                </View>
-            </TouchableOpacity>
-         
+            <View style = {{ justifyContent:"space-between", flexDirection: "row", height:cHeight, alignItems:"center", borderBottomWidth:1, borderColor:"#d4d3d0"}} >
+                <View  style = {{ flexDirection: "row", alignItems:"center", justifyContent: "center"}}>
+                    <AnimatedIcon                    
+                        style = {{ width: width*0.12, marginLeft:10 }}
+                        name = {iconName}
+                        color = {iconColor}
+                        size = {iconSize}
+                    />
+                    <TextInput 
+                        style = { [ labelStyle, {fontSize:labelSize, flex:1 } ] }
+                        underlineColorAndroid= "transparent"   
+                        placeholder = {hint}                     
+                        >{ label }</TextInput>                     
+                </View>                       
+            </View>
         );
     }
 }
--- src/pages/user/component/ProfileScreen.js
+++ src/pages/user/component/ProfileScreen.js
@@ -1,15 +1,121 @@
 import React,{Component} from "react";
 import {
     View,
-    Text
+    Text,
+    TouchableOpacity,
+    Image,
+    StyleSheet,
+    Dimensions,
+    ScrollView
 } from "react-native"
+import {StackNavigator} from "react-navigation"
+import CustomTextinput from "./CustomTextinput"
+// Some react native vector icon
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Foundation from 'react-native-vector-icons/Foundation';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+
+const { height, width } = Dimensions.get("window");
+const avatar_height = height*0.25;
 
 export default class ProfileScreen extends Component {
+
+    static navigationOptions = {
+        title: "Change my profile",
+        headerTitleStyle:{
+            alignSelf: "center",            
+            textAlign: "center",
+            width:"80%" ,
+            color:"#454545"           
+        },
+        headerRight:(
+            <TouchableOpacity style = {{ marginRight:10 }}>
+                <Text style= {{ fontSize:18, color: "#454545" }}>Done</Text>
+            </TouchableOpacity>
+        )            
+    };
+
     render(){
         return(
-         <View>
-            <Text>Change profile here</Text>
-         </View>
+         <ScrollView style = {styles.container}>
+            <View style = {styles.userView}>
+                <Image 
+                    style = {{ height:avatar_height*0.7, width:avatar_height*0.7, borderRadius:100 }} 
+                    source = {require("../../../img/1.jpg")}
+                /> 
+                <TouchableOpacity>
+                    <Text style = {{ fontSize: 18, color: "#75b4d2" }}>Change my avatar</Text>
+                </TouchableOpacity>                           
+            </View>
+
+            <View>
+                <CustomTextinput 
+                    iconClass={Entypo}
+                    iconName={"evernote"}
+                    label={"Lê Xuân Tiến"}
+                    hint="Name"
+                    /> 
+                <CustomTextinput 
+                    iconClass={Entypo}
+                    iconName={"evernote"}
+                    label={"shinigami"}
+                    hint="Nickname"/> 
+                <CustomTextinput 
+                    iconClass={Entypo}
+                    iconName={"evernote"}
+                    label={"facebook.com/tienlx97"}
+                    hint = "Website" />
+                <CustomTextinput 
+                    iconClass={Entypo}
+                    iconName={"evernote"}
+                    label={"student"}
+                    hint = "Job"
+                    /> 
+                <Text style = {{ textDecorationLine: "underline", fontSize:16 , marginLeft: 10, marginTop: 10}}>PRIVATE INFORMATION</Text>
+                <CustomTextinput 
+                    iconClass={Entypo}
+                    iconName={"evernote"}
+                    label={"tielx97@gmail.com"}
+                    hint = "Email"
+                    />
+                <CustomTextinput 
+                    iconClass={Entypo}
+                    iconName={"evernote"}
+                    label={"0932311434"}
+                    hint = "Phone"
+                    />  
+                <CustomTextinput 
+                    iconClass={Entypo}
+                    iconName={"evernote"}
+                    label={"male"}
+                    hint = "Gender"
+                    />  
+                    
+            </View>
+
+            <View>
+            
+            </View>
+
+         </ScrollView>
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    userView:{
+        height: avatar_height,
+        alignItems: 'center',
+        justifyContent: 'center', 
+        borderBottomWidth: 1,
+        borderColor:"#d4d3d0"
+    }
+
+});
