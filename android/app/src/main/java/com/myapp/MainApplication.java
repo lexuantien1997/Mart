de992006f44306823a209170804e65cdf478e3f5
LE Xuan Tien <lexuantien7617@outlook.com.vn>
2018-04-03 14:16:50 +0700

    fix facebook-sdk

--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -1,7 +1,10 @@
 package com.myapp;
 
 import android.app.Application;
-
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
@@ -14,6 +17,12 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -24,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new LinearGradientPackage()
+            new LinearGradientPackage(),
+              new FBSDKPackage(mCallbackManager)
       );
     }
 
